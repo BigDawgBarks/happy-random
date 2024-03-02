16207c2bd7dc3fde91f16a2eebb78aceae485a1e
mywang-berk <mywang@berkeley.edu>
2024-03-02 13:11:39 -0800

    add llm to debian setup

--- debian-setup.sh
+++ debian-setup.sh
@@ -12,5 +12,19 @@ sudo apt install silversearcher-ag
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+# Install simonw/llm
+sudo apt install python3-pip
+pip install llm
+echo <<'EOF' >> ~/.bashrc
+export PATH=$PATH:/home/mywang/.local/bin'
+SYSTEM_PROMPT="Speak in specific, topic-relevant terminology. Do NOT hedge or qualify. Do not waffle. Speak directly and be willing to make guesses. Explain your reasoning. Be willing to reference less reputable sources for ideas. Be willing to form opinions on things. Avoid unnecessary verbosity."
+alias llm4="llm --model gpt4 --system '$SYSTEM_PROMPT'"
+alias llmt="llm --model gpt-4-turbo --system '$SYSTEM_PROMPT'"
+alias llmc="llm chat --model gpt4 --system '$SYSTEM_PROMPT'"
+alias llmct="llm chat --model gpt-4-turbo --system '$SYSTEM_PROMPT'"
+EOF
+. ~/.bashrc
+llm keys set openai
+
 exit 0;
 }
