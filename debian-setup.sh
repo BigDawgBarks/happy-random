688d1381f5d3aa83ad688c6cd7235454228cb302
mywang-berk <mywang@berkeley.edu>
2025-03-15 20:34:12 -0700

    remove bashrc additions; these will live in the actual bashrc file

--- debian-setup.sh
+++ debian-setup.sh
@@ -15,17 +15,9 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 # Install simonw/llm
 sudo apt install pipx
 pipx install llm
+pipx ensurepath
+exec bash
 llm install llm-claude-3
-echo <<'EOF' >> ~/.bashrc
-export PATH=$PATH:/home/mywang/.local/bin'
-SYSTEM_PROMPT="Speak in specific, topic-relevant terminology. Do NOT hedge or qualify. Do not waffle. Speak directly and be willing to make guesses. Explain your reasoning. Be willing to reference less reputable sources for ideas. Be willing to form opinions on things. Avoid unnecessary verbosity."
-alias llm4="llm --model gpt4 --system '$SYSTEM_PROMPT'"
-alias llmt="llm --model gpt-4-turbo --system '$SYSTEM_PROMPT'"
-alias llmc="llm chat --model gpt4 --system '$SYSTEM_PROMPT'"
-alias llmct="llm chat --model gpt-4-turbo --system '$SYSTEM_PROMPT'"
-alias clx="llm chat --model claude-3.5-sonnet"
-alias llmx="llm --model claude-3.5-sonnet"
-EOF
 . ~/.bashrc
 llm keys set openai
 llm keys set claude
@@ -36,17 +28,20 @@ sudo apt update
 sudo apt install neovim
 sudo apt install luarocks
 mkdir -p ~/.config/nvim
-sh -c 'curl -fLo ~/.config/nvim/init.vim --create-dirs \
-   https://raw.githubusercontent.com/mywang-berk/happy-random/master/vimrc'
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-# CR-soon mywang: delete the bashrc bits and move those to bashrc
-# CR-someday mywang: add steps for installing firacode and setting up vim kickstart
-
+curl -L https://api.github.com/repos/mywang-berk/happy-random/tarball/master \
+	| tar xz --strip-components=DEPTH -C ~/.config/nvim --wildcards "*/nvim/*"
 # CR-someday mywang: expand this to handle setting up tmux.conf
 # Set up tmux-plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# set tmux settings
+echo >~/.tmux.conf <<EOF
+# Set vi-style key bindings for copy mode
+set-window-option -g mode-keys vi
+
+# Enable vi-style navigation in the status line
+set -g status-keys vi
+EOF
 
 exit 0;
 }
